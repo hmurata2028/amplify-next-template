cb06794cb391c6db5a57e9504d3e323ebd7b8ce2
murata-haruki <murata@sorcerer.co.jp>
2024-07-05 17:25:33 +0900

    fix: secretCodeを隠す

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,8 +8,8 @@ export const auth = defineAuth({
   loginWith: {
     externalProviders: {
       google: {
-        clientId: secret('22995177481-auud11boss9hvtchq22tdp5g66jg2mog.apps.googleusercontent.com'),
-        clientSecret: secret('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'),
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
       },
       callbackUrls: [
         'http://localhost:3000/', 'https://main.d3k27ywis1zirg.amplifyapp.com/'
